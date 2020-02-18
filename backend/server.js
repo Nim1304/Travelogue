c7951178ec167a644d79bc3ad013853a52b26738
Nim1304 <nimishshah2000@gmail.com>
2020-02-18 23:01:26 +0530

    commit on 'master'

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 const express=require('express');
 const cors=require('cors');
 const mongoose=require('mongoose');
-
+const connection = mongoose.connection;
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
 const app=express();
 
+const url='mongodb://127.0.0.1:27017';
+mongoose.connect(url,{useNewUrlParser:true,useCreateIndex:true});
+
+connection.once('open',()=>{
+    console.log('MongoDB connected');
+});
 
 app.listen(PORT,()=>{
     console.log(`listening on port ${PORT}`);
